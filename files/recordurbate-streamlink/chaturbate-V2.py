84e72ca9c00d4d8cb42cd86b1a03ad045dbaf7ec
fakedforyou <43475621+fakedforyou@users.noreply.github.com>
2026-06-08 21:00:23 -0400

    Create chaturbate-V2.py

---
+++ files/recordurbate-streamlink/chaturbate-V2.py
@@ -0,0 +1,67 @@
+import re
+import uuid
+
+from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.plugin.api import validate
+from streamlink.stream.hls import HLSStream
+
+API_HLS = "https://chaturbate.com/get_edge_hls_url_ajax/"
+
+_url_re = re.compile(
+    r"https?://(\w+\.)?chaturbate\.com/(?P<username>\w+)"
+)
+
+_post_schema = validate.Schema(
+    {
+        "url": str,
+        "room_status": str,
+        "success": int,
+    }
+)
+
+
+@pluginmatcher(_url_re)
+class Chaturbate(Plugin):
+    def _get_streams(self):
+        match = self.match
+        username = match.group("username")
+
+        CSRFToken = str(uuid.uuid4().hex.upper()[0:32])
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-CSRFToken": CSRFToken,
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": self.url,
+        }
+
+        cookies = {
+            "csrftoken": CSRFToken,
+        }
+
+        post_data = "room_slug={0}&bandwidth=high".format(username)
+
+        data = self.session.http.post(
+            API_HLS,
+            headers=headers,
+            cookies=cookies,
+            data=post_data,
+            schema=_post_schema,
+        )
+
+        self.logger.info(
+            "Stream status: {0}".format(data["room_status"])
+        )
+
+        if (
+            data["success"] is True
+            and data["room_status"] == "public"
+            and data["url"]
+        ):
+            return HLSStream.parse_variant_playlist(
+                self.session,
+                data["url"],
+            )
+
+
+__plugin__ = Chaturbate
